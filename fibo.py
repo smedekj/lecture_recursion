0d2c815f16705eeaaab18da26a0338269ce4eb2b
smedekj <smedek.j@seznam.cz>
2025-04-28 13:17:08 +0200

    prvni

--- fibo.py
+++ fibo.py
@@ -1,6 +1,15 @@
+def recursive_nth_fibo(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
+    hodnota = recursive_nth_fibo(6)
+    print(hodnota)
 
 
 if __name__ == "__main__":
